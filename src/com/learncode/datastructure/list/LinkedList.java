54ba235846f4ab7ff583eb6b064c9b63e15140ab
elielzamora <elielzamora@gmail.com>
2016-01-28 00:40:25 -0500

    Adds methods like appendAfter, deleteAfter to LinkedList
    Also starts out code for brute force O(n^2) method for finding max
    sub array.
    
    O(nlogn) and O(n) to follow.

--- src/com/learncode/datastructure/list/LinkedList.java
+++ src/com/learncode/datastructure/list/LinkedList.java
@@ -3,12 +3,29 @@ package com.learncode.datastructure.list;
 import com.learncode.datastructure.list.Node;
 import java.util.*;
 
+
 public class LinkedList <Item>{
 	Node<Item> head;
 	
+	/**
+	 * constructor for linked list creates one node to 
+	 * begin with
+	 * @param i
+	 */
 	public LinkedList(Item i){
 		this.head = new Node<Item>(i, null);
 	}
+	/**
+	 * appends a node to the head
+	 * @param item
+	 */
+	public void appendToHead(Item item){
+		this.head = new Node<Item>(item, this.head);
+	}
+	/**
+	 * appends to tail
+	 * @param item
+	 */
 	public void append(Item item){
 		Node<Item> temp = this.head;
 		while(temp.hasNext()){
@@ -16,9 +33,55 @@ public class LinkedList <Item>{
 		}
 		temp.setNext(new Node<Item>(item, null));
 	}
-	public int search(){
-		//TODO return position as a number :)
-		return 0;
+	/**
+	 * appends in the middle of the list after some fixed value 
+	 * 
+	 */
+	public void appendAfter(int i, Item item){
+		Node<Item> node = this.head;
+		for(int x = 0; x < i; x++){
+			if(node.next() != null) node = node.next();
+			//else throw new exception 
+		}
+		if(node.next() != null)	{
+			node.setNext(new Node<Item>(item, node.next()));
+		}
+	}
+	/**
+	 * return position in list as an integer 0 based
+	 * where 0 is the head of the list
+	 * @param item
+	 * @param comp
+	 * @return
+	 */
+	public int search(Item item, Comparator<Item> comp){
+		// max running time 
+		int counter = 0;
+		Node<Item> node = this.head;
+		while(node.hasNext() == true){
+			if(comp.compare(item, node.getItem()) != 0){
+				return counter;
+			}
+			counter ++;
+		}
+		return -1; // no results found // or return exception
+	}
+	/**
+	 * the head is not deleted but the node is moved
+	 * and replaced by head.next
+	 */
+	public void deleteHead(){
+		this.head = this.head.next();
+	}
+	public void deleteAfter(int i){
+		Node<Item> node = this.head;
+		for(int x = 0; x < i; x++){
+			if(node.next() != null) node = node.next();
+			//else throw new exception 
+		}
+		if(node.next().next() != null)	{
+			node.setNext(node.next().next());
+		}
 	}
 	//TODO finish linked list iterator functionality
 	private class LinkedListIterator implements Iterator{
@@ -34,12 +97,14 @@ public class LinkedList <Item>{
 			return null;
 		}
 	}
-	//TODO add iterator , add delete, add search, add more methods
+	public void clear(){
+		this.head = null;
+	}
 	/**
 	 * unit test 
 	 * @param args
 	 */
 	public static void main(String... args){
-		
+		//TODO unit test
 	}
 }
--- src/com/learncode/misc/MaxSubArray.java
+++ src/com/learncode/misc/MaxSubArray.java
@@ -1,12 +1,30 @@
 package com.learncode.misc;
+
 import java.util.ArrayList;
+
 public class MaxSubArray {
 	public ArrayList<Integer> array;
+	public static final int LENGTH = 20;
 	public MaxSubArray(){
-		//init random array
+		//this();
+	}
+	
+	public MaxSubArray(ArrayList<Integer> array){
+		this.array = array;
 	}
 	public void bruteForce(){
-		
+		int size = this.array.size();
+		int a = 0, b =  0, max = 0, total = 0;		
+		for(int i = 0; i < size; i++){
+			total = 0;
+			for(int j = i; j < size; j++){
+				if(total > max){
+					max = total;
+					a = i;
+					b = j;
+				}
+			}
+		}
 	}
 	
 	/**
